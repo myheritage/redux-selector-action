220ddecb2fc4693c469559d46257a3aa8317ca56
peri.levy <peri.levy@myheritage.com>
2020-07-16 13:34:37 +0300

    Revert "Add package-lock"
    This reverts commit c30fb1836a4a7e92d126e0b6581c86a85b0fdc3b.

--- src/index.js
+++ src/index.js
@@ -8,11 +8,7 @@ export function ReduxActionSelectorMiddleware(store) {
 	return next => action => {
 		const {type, payload, meta} = action;
 		
-		if (type === REDUX_ACTION_SELECTOR) {
-            if (typeof payload !== 'function') {
-                throw new Error('');
-            }
-
+		if (type === REDUX_ACTION_SELECTOR && typeof meta === 'object' && typeof payload === 'function') {
             const {selectors, args} = meta;
             const state = store.getState();
             const computedSelectors = selectors.map(selector => typeof selector === 'function' ? selector(state) : selector);
@@ -24,24 +20,6 @@ export function ReduxActionSelectorMiddleware(store) {
 	}
 }
 
-
-/**
- * Accepts an action creator and selectors which we want to inject as dependencies of the action creator.
- * A dependency can be a selector function, which its output will be passed or any other value.
- * Supports the fullowing formats:
- * 1. createActionSelector(dep1, dep2, ..., actionCreator)
- * 2. createActionSelector([dep1, dep2, ...], actionCreator)
- * Returns a new action creator which we can dispatch with redux store.dispatch.
- * The new action creator can be tested using its new props resultFunc, dependencies.
- * resultFunc = the given action creator function
- * dependencies = the given selectors as an array
- * 
- * We can use a special selector as a dependency, called getPlaceholder, for supporting args of the returned action creator.
- * Each getPlaceholder saves the argument position for another outer argument.
- * Once we dispatch the new action, its args will fill all the placeholders by their position order.
- * 
- * @param  {...function} funcs 
- */
 export function createActionSelector(...funcs) {
     const actionCreator = funcs.pop();
     const selectors = Array.isArray(funcs[0]) ? funcs[0] : funcs;
